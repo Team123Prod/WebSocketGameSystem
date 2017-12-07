a566994eddd01957147408351bf0567fc1b01099
Mironova Ekaterina <ekmironova123@gmail.com>
2017-12-07 21:56:48 +0200

    DbMock

---
+++ WebSocketServer/DAO/IUserAccountDAO.cs
@@ -0,0 +1,17 @@
+﻿using GameServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.DAO
+{
+    interface IUserAccountDAO
+    {
+        void Create(UserAccount p);
+        void Delete(UserAccount p);
+        List<UserAccount> Read();
+        void Update(UserAccount p);
+    }
+}
