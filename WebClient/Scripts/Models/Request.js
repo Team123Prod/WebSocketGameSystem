746bf2d087dcfa9ddfefd8fff79af1d4f0590914
Mironova Ekaterina <ekmironova123@gmail.com>
2017-12-07 22:17:47 +0200

    mock

--- WebClient/Scripts/Models/Request.js
+++ WebClient/Scripts/Models/Request.js
@@ -1,9 +1,16 @@
 class Request {
-	constructor(cmd, module, idRoom, player, idMove) {
-    this.cmd = cmd;
-    this.module = module;
-	this.idRoom = idRoom;
-	this.player = player;
-	this.idMove = idMove;
-  }
+    constructor(cmd, module, args) {
+        this.cmd = cmd;
+        this.module = module;
+        this.args = args;
+    }
 }
+//class Request {
+//	constructor(cmd, module, idRoom, player, idMove) {
+//    this.cmd = cmd;
+//    this.module = module;
+//	  this.idRoom = idRoom;
+//	  this.player = player;
+//	  this.idMove = idMove;
+//  }
+//}
--- WebClient/Scripts/Models/engine.js
+++ WebClient/Scripts/Models/engine.js
@@ -13,14 +13,14 @@ function aiBot() {
 }
 
 function checkEnd() {
-  if (t[0]=='ai' && t[1]=='ai' && t[2]=='ai' || t[0]=='player' && t[1]=='player' && t[2]=='player')  return true;
-  if (t[3]=='ai' && t[4]=='ai' && t[5]=='ai' || t[3]=='player' && t[4]=='player' && t[5]=='player')  return true;
-  if (t[6]=='ai' && t[7]=='ai' && t[8]=='ai' || t[6]=='player' && t[7]=='player' && t[8]=='player')  return true;
-  if (t[0]=='ai' && t[3]=='ai' && t[6]=='ai' || t[0]=='player' && t[3]=='player' && t[6]=='player')  return true;
-  if (t[1]=='ai' && t[4]=='ai' && t[7]=='ai' || t[1]=='player' && t[4]=='player' && t[7]=='player')  return true;
-  if (t[2]=='ai' && t[5]=='ai' && t[8]=='ai' || t[2]=='player' && t[5]=='player' && t[8]=='player')  return true;
-  if (t[0]=='ai' && t[4]=='ai' && t[8]=='ai' || t[0]=='player' && t[4]=='player' && t[8]=='player')  return true;
-  if (t[2]=='ai' && t[4]=='ai' && t[6]=='ai' || t[2]=='player' && t[4]=='player' && t[6]=='player')  return true;
+  if (t[0]==='ai' && t[1]==='ai' && t[2]==='ai' || t[0]==='player' && t[1]==='player' && t[2]==='player')  return true;
+  if (t[3]==='ai' && t[4]==='ai' && t[5]==='ai' || t[3]==='player' && t[4]==='player' && t[5]==='player')  return true;
+  if (t[6]==='ai' && t[7]==='ai' && t[8]==='ai' || t[6]==='player' && t[7]==='player' && t[8]==='player')  return true;
+  if (t[0]==='ai' && t[3]==='ai' && t[6]==='ai' || t[0]==='player' && t[3]==='player' && t[6]==='player')  return true;
+  if (t[1]==='ai' && t[4]==='ai' && t[7]==='ai' || t[1]==='player' && t[4]==='player' && t[7]==='player')  return true;
+  if (t[2]==='ai' && t[5]==='ai' && t[8]==='ai' || t[2]==='player' && t[5]==='player' && t[8]==='player')  return true;
+  if (t[0]==='ai' && t[4]==='ai' && t[8]==='ai' || t[0]==='player' && t[4]==='player' && t[8]==='player')  return true;
+  if (t[2]==='ai' && t[4]==='ai' && t[6]==='ai' || t[2]==='player' && t[4]==='player' && t[6]==='player')  return true;
   if(t[0] && t[1] && t[2] && t[3] && t[4] && t[5] && t[6] && t[7] && t[8]) return true;
 }
 
@@ -30,9 +30,9 @@ function move(id, role) {
   document.getElementById(id).className = 'cell ' + role;
   var checkBox = document.getElementById('isBot');
   if(checkBox.checked){
-	!checkEnd() ? (role == 'player') ? aiBot() : null : reset()
+	!checkEnd() ? (role === 'player') ? aiBot() : null : reset()
   }else{
-	!checkEnd() ? (role == 'player') ? go(id) : null : reset()
+	!checkEnd() ? (role === 'player') ? go(id) : null : reset()
   }
 }
 
