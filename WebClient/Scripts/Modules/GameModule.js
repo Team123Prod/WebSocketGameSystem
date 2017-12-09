784fa1de8c2fdb6d5d0979545c2acbedb2c61388
Mironova Ekaterina <ekmironova123@gmail.com>
2017-12-09 23:25:49 +0200

    added GameModule.js with dispatcher

---
+++ WebClient/Scripts/Modules/GameModule.js
@@ -0,0 +1,25 @@
+﻿var ws;
+
+function GameModule(request) {
+    switch (request.cmd) {
+        case "startGame":
+            Start(request);
+            break;
+        case "move":
+            Move(request);
+            break;
+    }
+}
+
+function Start(request)
+{
+    var reqJson = JSON.stringify(new Request("GameModule", "GameStarted", request.args))
+    ws = new Connection();
+    ws.send(reqJson);
+}
+
+function Move(request) {
+    var reqJson = JSON.stringify(new Request("GameModule", "move", request.args))
+    ws = new Connection();
+    ws.send(reqJson);
+}
